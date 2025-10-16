67c289ae78bdb23408b7f49d6dd2058eac43d772
Hanni <hannilea23@gmail.com>
2025-10-16 11:17:27 +0200

    Flytta befintligt projekt till Project1-mapp

---
+++ Project1/Models/Event.cs
@@ -0,0 +1,10 @@
+public class Event
+{
+    public int ID { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public Date Date { get; set; }
+    public string Time { get; set; }
+    public string Location { get; set; }
+    public string Category { get; set; }
+}
