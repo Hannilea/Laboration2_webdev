2e347f684846eaf065db930d94ba84549e20d6d2
Hanni <hannilea23@gmail.com>
2025-10-18 16:04:05 +0200

    create event model

--- TownSquareAuth/Models/Event.cs
+++ TownSquareAuth/Models/Event.cs
@@ -8,12 +8,13 @@ namespace TownSquareAuth.Models
     public class Event
     {
         public int Id { get; set; }
-        public string Title { get; set; }
-        public string description { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+
         public DateTime Date { get; set; }
         public string Time { get; set; }
-        public string Location { get; set; }
-        public string Category { get; set; }
+        public string Location { get; set; } = string.Empty;
+        public string Category { get; set; } = string.Empty;
 
     }
 }
