87664286df615c577bffa596cfb7a9f5b2cb7494
Hanni <hannilea23@gmail.com>
2025-10-18 16:05:45 +0200

    add event dbset to applicationdbcontext

--- TownSquareAuth/Data/ApplicationDbContext.cs
+++ TownSquareAuth/Data/ApplicationDbContext.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using TownSquareAuth.Data.Migrations;
+using TownSquareAuth.Models;
 
 namespace TownSquareAuth.Data;
 
@@ -9,4 +11,6 @@ public class ApplicationDbContext : IdentityDbContext
         : base(options)
     {
     }
+
+    public DbSet<Event> Events { get; set; }
 }
